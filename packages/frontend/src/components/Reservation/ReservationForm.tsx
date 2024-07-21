8e3242f41fe05889f3e9787115aa9f55ba8a78b6
Han.Lin <hlin2@opentext.com>
2024-07-21 12:56:22 +0800

    add constraint

--- packages/frontend/src/components/Reservation/ReservationForm.tsx
+++ packages/frontend/src/components/Reservation/ReservationForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { TextField, Grid, Button } from '@mui/material';
+import { TextField, Grid, Button, Container, Typography, Box } from '@mui/material';
 import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { CREATE_RESERVATION } from '../queries/mutations';
@@ -39,51 +39,73 @@ const ReservationForm: React.FC = () => {
     };
 
     return (
-        <div>
-            <h2>Create Reservation</h2>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        label="Guest Name"
-                        value={guestName}
-                        onChange={(e) => setGuestName(e.target.value)}
-                        fullWidth
-                    />
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h4">Create Reservation</Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={15}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            label="Guest Name"
+                            inputProps={{ maxLength: 127 }}
+                            value={guestName}
+                            onChange={(e) => setGuestName(e.target.value)}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={15}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            label="Guest Contact"
+                            inputProps={{ maxLength: 127 }}
+                            value={guestContact}
+                            onChange={(e) => setGuestContact(e.target.value)}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={15}>
+                        <DatePicker
+                            required
+                            selected={arrivalTime}
+                            onChange={(date: Date | null) => setArrivalTime(date)}
+                            minDate={new Date()}
+                            customInput={<TextField label="Arrival Time" fullWidth />}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={15}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            type="number"
+                            label="Table Size"
+                            inputProps={{ min: 1, max: 10 }}
+                            value={tableSize}
+                            onChange={(e) => setTableSize(Number(e.target.value))}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary" onClick={handleSubmit}>
+                            Create
+                        </Button>
+                        <Button variant="outlined" onClick={handleBack}>
+                            Back
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        label="Guest Contact"
-                        value={guestContact}
-                        onChange={(e) => setGuestContact(e.target.value)}
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <DatePicker
-                        selected={arrivalTime}
-                        onChange={(date: Date | null) => setArrivalTime(date)}
-                        customInput={<TextField label="Arrival Time" fullWidth />}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        type="number"
-                        label="Table Size"
-                        value={tableSize}
-                        onChange={(e) => setTableSize(Number(e.target.value))}
-                        fullWidth
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained" color="primary" onClick={handleSubmit}>
-                        Submit
-                    </Button>
-                    <Button variant="outlined" onClick={handleBack}>
-                        Back
-                    </Button>
-                </Grid>
-            </Grid>
-        </div>
+            </Box>
+        </Container>
     );
 };
 
